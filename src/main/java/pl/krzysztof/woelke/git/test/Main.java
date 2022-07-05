88782c7ec8df96fbc8350096281add849afee7ca
Krzysiek <k_woelke@o2.pl>
2022-07-05 17:27:17 +0200

    test add new line

--- src/main/java/pl/krzysztof/woelke/git/test/Main.java
+++ src/main/java/pl/krzysztof/woelke/git/test/Main.java
@@ -1,7 +1,10 @@
 package pl.krzysztof.woelke.git.test;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test git");
+        System.out.println("New test");
     }
 }
